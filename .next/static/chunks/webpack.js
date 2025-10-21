78eaeb5be76366b65cb25e23779abadae900fd5f
Kenan Turgay <knnturgay@gmail.com>
2025-10-21 23:39:50 +0300

    Refactor code structure for improved readability and maintainability

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "39a7fc16bf2782a2"; }
+/******/ 		__webpack_require__.h = function() { return "7b6932af217d623e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
